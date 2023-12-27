889a2628faa416ad37b12ca4b109105a0f05caf2
JairSDS <jair@sdsharp.com>
2023-12-27 11:59:55 -0500

    short decimal in detail view

--- frontend/pages/home/components/ICRC/SubaccountAction.tsx
+++ frontend/pages/home/components/ICRC/SubaccountAction.tsx
@@ -25,7 +25,7 @@ const ICRCSubaccountAction: FC<ICRCSubaccountActionProps> = ({ onActionClick }) 
             <p className="font-semibold text-[1.15rem] text-right">{`${toFullDecimal(
               selectedAccount?.amount || "0",
               selectedAccount?.decimal || 8,
-              8,
+              Number(selectedAsset?.shortDecimal),
             )} ${selectedAsset?.symbol || ""}`}</p>
             <p className="font-semibold text-md">{`$${selectedAccount?.currency_amount || ""}`}</p>
           </div>
